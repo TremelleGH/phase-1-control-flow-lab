eede2a5ae77690880d03a504c532e98811c49955
Tremelle Ancrum <tancrum1996@gmail.com>
2022-04-27 00:43:39 -0400

    commit tree 12b25010c6faa6b610f4dc0a9c808cfb14767379

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= '400') {
+    return "This one is on me!"
+  } 
+  else if (ride < '2000') {
+      return  "That will be twenty bucks."
+  }
+  else if (ride > '2500') {
+    return "No can do."
+  }
+  else {
+    return "I will gladly take your thirty bucks."
+  }
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC'?'Ok, sounds good.' :'No go.'
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+  
 }
